b8fea3c0aea6733fcce4f022d22aad7027caecf4
Rosculet <rousculet89@gmail.com>
2023-06-08 20:14:31 +0200

    add Authentication

--- src/main/java/com/example/bank_security/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/bank_security/config/ProjectSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,7 +18,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {return NoOpPasswordEncoder.getInstance();}
+    public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
--- src/main/java/com/example/bank_security/controller/LoginController.java
+++ src/main/java/com/example/bank_security/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.example.bank_security.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @Autowired
-    CustomerRepository customerRepository;
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     public ResponseEntity<String>registerUse(@RequestBody CustomerEntity customer){
         CustomerEntity savedCustomer = null;
         ResponseEntity responseEntity = null;
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
-            if (savedCustomer.getId()>0){
+            if (
+                    savedCustomer.getId()>0){
                 responseEntity = ResponseEntity
                         .status(HttpStatus.CREATED)
                         .body("Given user details are successfully registered");
